ae437af715a93c213952e106ad2bceb9708915d2
Nimit Kalra <nimit@nimitkalra.com>
2015-04-18 01:50:33 -0500

    Set parameters from what is specified based on rules.

--- __init__.py
+++ __init__.py
@@ -36,11 +36,43 @@ alias.write('\tesac')
 alias.write('\n\n\techo');
 for argument in data['arguments']:
 	alias.write(' $' + argument + 'Set')
+alias.write('\n')
+
+for index, argument in enumerate(data['arguments']):
+	alias.write('\n\tif $' + argument + '; then\n\t\t')
+	if data['blueprint'][data['blueprint'].index(argument) - 1] == '*':
+		alias.write(argument + '=${parameters[' + str(index) + ']}')
+	else:
+		alias.write('case ${parameters[' + str(index) + ']} in')
+
+		for value in data['arguments'][argument]['values']:
+			if value != 'default':
+				alias.write('\n\t\t\t"' + value + '" ) ' + argument + '="' + data['arguments'][argument]['values'][value] + '";;\n')
+
+		invalidValueError = data['arguments'][argument]['errors']['invalid']
+		for invalidValueErrorArgumentIndex, invalidValueErrorArgument in enumerate(data['arguments']):
+			invalidValueError = invalidValueError.replace('{' + invalidValueErrorArgument + '}', '${parameters[' + str(invalidValueErrorArgumentIndex) + ']}')
+
+		alias.write('\n\t\t\t* )' + ' echo "' + invalidValueError + '";;')
+
+		alias.write('\n\t\tesac')
+
+	alias.write('\n\telse')
+	alias.write('\n\t\t' + argument + '="' + data['arguments'][argument]['values']['default'] + '"')
+	alias.write('\n\tfi')
+
+	alias.write('\n')
+
+# Generate command from blueprint and data.
+command = data['blueprint']
+
+# Call the blueprint's command.
+# alias.write('\n' + str(command))
 
 # Close the function declration.
 alias.write('\n}')
 
 # Call the function...
-alias.write('\n' + blueprint[0] + ' $@')
+alias.write('\n\n' + blueprint[0] + ' $@')
 
 alias.close()
